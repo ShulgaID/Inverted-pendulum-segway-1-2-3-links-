57d25bfc373e5a590b68ef81f7aba324620d0fb3
careydee <157249572+careydee@users.noreply.github.com>
2026-01-21 17:05:26 +0300

    Refactor main.py to include LQRController and parameters

--- main.py
+++ main.py
@@ -1,32 +1,55 @@
+import numpy as np
 from controllers.pid import PIDController
+from controllers.lqr import LQRController
 from simulation.simulation import Simulation
+
+MODEL_PATH = "pendulum.xml"
+SIM_TIME = 10.0
+TIME_STEP = 0.001
+
+# параметры ПИД-регулятора
+KP = 120
+KI = 5
+KD = 25
+INT_LIMIT = 1000
+ANGLE_WEIGHTS = [50, 10, 100]
+VELOCITY_WEIGHTS = [10, 20, 50]
+
+# параметры LQR-регулятора
+Q = np.diag([
+    0.5, 0.05,   # карт
+    5.0, 0.5,    # 1 звено
+    4.0, 0.4,    # 2 звено
+    3.0, 0.3])   # 3 звено
+R = np.array([[50]])
 
 def main():
     
-    controller = PIDController(
-        dt=0.001,
-        kp=10000,
-        ki=1,
-        kd=10,
-        int_limit=1000,
-        ang_weights=[50, 10, 100],
-        vel_weights=[10, 20, 50])
+    simulation = Simulation(
+    model_path=MODEL_PATH,
+    sim_end=SIM_TIME)
     
     """
-    controller = LQRController(
-        dt=0.001,
-        Q=,
-        R=)
+    controller = PIDController(
+        dt=TIME_STEP,
+        kp=KP,
+        ki=KI,
+        kd=KD,
+        int_limit=INT_LIMIT,
+        ang_weights=ANGLE_WEIGHTS,
+        vel_weights=VELOCITY_WEIGHTS)
     """
-
-    simulation = Simulation(
-        model_path="pendulum.xml",
-        sim_end=50.0,
-        controller=controller)
+    controller = LQRController(
+        Q=Q,
+        R=R,
+        model=simulation.model,
+        data=simulation.data)
+    #"""
 
     simulation.initialize()
-    simulation.run()
+    simulation.run(controller)
     simulation.logger.save_csv("pendulum_control.csv")
+    simulation.logger.show()
 
 
 if __name__ == "__main__":
